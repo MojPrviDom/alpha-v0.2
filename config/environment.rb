624a06c8179bd0c1dd3a2b0219eca26bcc95920e
alojzije <shljunak@hotmail.com>
2014-04-06 16:31:33 +0200

    Make new rails app

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+MojPrviDomV2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MojPrviDomV2::Application.config.secret_key_base = '2a0f692d536b6555fafc56ef1b3c358937a93f4eac3e4eed7196fd434baf50cdd643ddaf12ede8690bfa1592d75611c69f26786f46fe7c5c13ae49bfbba7d692'
