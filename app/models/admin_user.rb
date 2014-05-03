5586e40ac9661adc4cac534de952a2b32406a4a0
alojzije <shljunak@hotmail.com>
2014-05-03 15:48:31 +0200

    Add (in models/admin_user.rb) a  model check that doesn't allow deleting a user it it's the only one

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,15 @@ class AdminUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_destroy :check_number_of_users
+
+  def check_number_of_users
+  	# does not allow deleting AdminUser if there is only one
+  	if AdminUser.all.count <= 1
+  		self.errors[:base] = "Cannot delete the only user."
+    	return false   
+	end
+  end
+
 end
