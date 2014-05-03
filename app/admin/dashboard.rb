7f4516fed68a773cc63854892e88ec3e4c8a6966
alojzije <shljunak@hotmail.com>
2014-05-03 15:45:00 +0200

    Modify admin/dashboard.rb to list posts on dashboard's home page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,28 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+    panel "Recent Posts" do
+       table_for Post.order('created_at DESC').limit(5) do
+          column :title do |post|
+            link_to post.title, [:admin, post]
+          end
+          column :body do |post|
+            truncate(post.body)
+          end
+          column "update Date", :updated_at do |post|
+            post.updated_at.strftime('%m.%d.%Y.   %k:%M')
+          end      
+        end
+      strong {link_to "View All posts", admin_posts_path}
     end
+
+
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
