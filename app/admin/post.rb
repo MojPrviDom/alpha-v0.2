472cfd2d177eb99718dc576f3fe95a6a31fe7bbf
alojzije <shljunak@hotmail.com>
2014-05-03 15:42:27 +0200

    Modify admin/posts.rb for custom post listing

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,6 +1,17 @@
 ActiveAdmin.register Post do
   permit_params :title, :body
   
+  index do
+    column :title
+    column :body do |post|
+      truncate(post.body)
+    end
+    column "update Date", :updated_at do |post|
+      post.updated_at.strftime('%m.%d.%Y.   %k:%M')
+    end
+    default_actions
+  end
+  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
