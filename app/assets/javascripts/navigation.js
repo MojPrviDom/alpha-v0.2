ba4b2eea7ff322ce64f99e791314ce14bb213d5d
alojzije <shljunak@hotmail.com>
2014-05-24 23:50:15 +0200

    Add navigation.js for toggling navigation

---
+++ app/assets/javascripts/navigation.js
@@ -0,0 +1,12 @@
+$(document).on('page:change',function() {
+	$(".navigation_logo").on('click', function() {	// Big screen navigation bar toogle
+		$(".navigation_box").slideToggle( "fast");	// When clicked navigation bar slides down or up
+	});
+
+	var menu = $('nav ul');						// Small screen navigation bar toogle
+	$('#pull').on('click', function() {
+		menu.slideToggle('fast');				// When clicked, small navigation bar slides down
+	});
+});
+
+
